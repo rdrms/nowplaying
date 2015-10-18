f1cffc359383ba3de96a819a4113a21ca5711d23
ryanrms <ryanrms@gmail.com>
2015-10-18 00:04:34 -0600

    Adding comments to playing.sh

--- playing.sh
+++ playing.sh
@@ -2,9 +2,11 @@
 
 #set our variables
 mpdnp="$(/usr/local/bin/mpc | grep -)"
-spotifynp="$($HOME/scripts/SpotifyControl info)"
+spotifynp="$($HOME/scripts/SpotifyControlModified info)"
 foo=0
 foo2=0
+
+#check if mpd is playing, if it is return the current song
 
 /usr/local/bin/mpc | grep 'playing' &> /dev/null
 if [ $? == 0 ]; then
@@ -13,14 +15,16 @@ else
 	foo=1
 fi
 
+#check if Spotify is playing, if it is, return the current song
 
-$HOME/scripts/SpotifyControlOriginal info | grep 'playing' &> /dev/null
+$HOME/scripts/SpotifyControl info | grep 'playing' &> /dev/null
 if [ $? == 0 ]; then
 	echo $spotifynp
 else 
 	foo2=1
 fi
 
+#If neither is playing, return "Not Playing."
 if [ $foo -eq 1 ] && [ $foo2 -eq 1 ]; then
 	echo "Not Playing"
 fi
